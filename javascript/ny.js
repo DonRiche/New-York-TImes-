a1048bc597084753ecdaa458bc7a23c6f7338f0f
Colin <viewtifuljho@hotmail.com>
2019-07-06 12:29:25 -0500

    initial search term

---
+++ javascript/ny.js
@@ -0,0 +1,1 @@
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&begin_date=0101" + startYear + "&end_date=0101" + endYear + "&api-key=ZVaT9Hn0MgGveFG8hw7Avnr9Zjk089GX"
