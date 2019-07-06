68a59aa037d335f1c970c274444a822709057945
Colin <viewtifuljho@hotmail.com>
2019-07-06 13:43:08 -0500

    added article class

--- javascript/ny.js
+++ javascript/ny.js
@@ -1,23 +1,49 @@
 
 var keyword = "george bush";
 
-var startYear = "2000";
+var startYearField = "";
 
-var endYear = "2010";
+var startYearNumber = "2000";
+
+var startYear = "&begin_date=" + startYearNumber;
+
+var endYearField = "";
+
+var endYearNumber = "2010";
+
+var endYear = "0101&end_date=" + endYearNumber;
 
 var resultsNumber = 5
 
-var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + "&begin_date=" + startYear + "0101&end_date=" + endYear + "1231&api-key=ZVaT9Hn0MgGveFG8hw7Avnr9Zjk089GX"
+var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + keyword + startYear + endYear + "1231&api-key=ZVaT9Hn0MgGveFG8hw7Avnr9Zjk089GX"
 
 $.ajax({
     url: queryURL,
     method: "GET"
   }).then(function(response) {
     for(var i = 0; i < resultsNumber; i++){
+        if (startYearField !== ""){
+            startYear = "&begin_date=" + startYearNumber + "0101";
+        }
+        
+        else{
+            startYear = "";
+        }
+
+        if (endYearField !== ""){
+            endYear = "&end_date=" + endYearNumber + "1231";
+        }
+        
+        else{
+            endYear = "";
+        }
+
+        
         var headline = (response.response.docs[i].headline.main);
         var byline = (response.response.docs[i].byline.main);
         console.log(response.response.docs[i].web_url)
         var articleDiv = $("<div>");
+        articleDiv.addClass("article");
         
         articleDiv.append(headline);
         articleDiv.append(byline);
